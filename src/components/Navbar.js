3d68327c8086af254ff24599dbc615c86bf2fbc5
Mohammed Khallaf <mohammed.khallaf.1997@gmail.com>
2022-02-06 04:53:28 +0200

    add navbar style

---
+++ src/components/Navbar.js
@@ -0,0 +1,25 @@
+import { Nav, Navbar as BNavbar, Container } from "react-bootstrap";
+import { Link,NavLink } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <header>
+      <BNavbar bg="dark" variant="dark">
+        <Container fluid className="mx-3">
+          <BNavbar.Brand href="#home">My Book Shelf</BNavbar.Brand>
+          <Nav className="ms-auto ">
+            <NavLink to="/" className="nav-link">
+                Home
+                </NavLink>
+                <NavLink to="/search" className="nav-link">
+                Search
+                </NavLink>
+          </Nav>
+        </Container>
+      </BNavbar>
+    
+    </header>
+  );
+};
+
+export default Navbar;
